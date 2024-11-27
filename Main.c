61654f0cbdf6d4fbec6a470145d3791de578576d
victor garcia <victorhugogamz@gmail.com>
2024-11-26 23:10:29 -0700

    Menu and parse_int v2

--- Main.c
+++ Main.c
@@ -11,39 +11,81 @@ bool parse_int(char *str, int *integer);
 int main() {
 
     bool parsed_correct = true;
-    int integer = 0;
+    int integer;
+    bool opcion = false;
+    bool avanzar = false;
     FILE *Xfile;
     Xfile=fopen("agenda.cf","r+");
 
-
     if(Xfile==NULL) {
-        printf("Error opening file");
+        printf("");
+        printf("\nError opening file");
+        Xfile=fopen("agenda.cf","w");
 
     } else {
-        printf("correctly opened file");
+        printf("\ncorrectly opened file");
     }
 
-    do {
-        printf("\nMenu de opciones");
-        printf("\n1.- Agregar contacto");
-        printf("\n2.- Buscar contacto");
-        printf("\n3.- Editar contacto");
-        printf("\n4.- Eliminar contacto");
-        printf("\n5.- Listar contactos");
-        printf("\n6.- Salir");
-        printf("\n\nIngrese la opcion deseada: ");
+    printf("\nMenu de opciones");
+    printf("\n1.- Agregar contacto");
+    printf("\n2.- Buscar contacto");
+    printf("\n3.- Editar contacto");
+    printf("\n4.- Eliminar contacto");
+    printf("\n5.- Listar contactos");
+    printf("\n6.- Salir");
+    printf("\n\nIngrese la opcion deseada: ");
 
+    do {
         char buffer[BUFFER_SIZE];
         fgets(buffer, BUFFER_SIZE, stdin);
-
+        
         parsed_correct = parse_int(buffer, &integer);
 
-        if(!parsed_correct)
-            printf("Favor de ingresar el valor correcto de la opcion deseada\n");
+        if ((integer < 7) && (integer > 0)) opcion = true;
 
-    }while(!parsed_correct);
+        if(!parsed_correct || (!opcion)){
+            printf("\nFavor de ingresar el valor correcto de la opcion deseada: ");
+            avanzar = true;
+        }else{
+            avanzar = false;
+        }
+
+    }while(avanzar);
 
     printf("Opcion seleccionada: %d\n", integer);
+
+    switch (integer)
+    {
+    case 1:
+        printf("\n*-*-*-*-*-*Agregar contacto*-*-*-*-*-*");
+        printf("\n*-*-*-*-*-*Work in progress*-*-*-*-*-*");
+        break;
+
+    case 2:
+        printf("\n*-*-*-*-*-*Buscar contacto-*-*-*-*-*-*");
+        printf("\n*-*-*-*-*-*Work in progress*-*-*-*-*-*");
+        break;
+
+    case 3:
+        printf("\n*-*-*-*-*-*Editar contacto-*-*-*-*-*-*");
+        printf("\n*-*-*-*-*-*Work in progress*-*-*-*-*-*");
+        break;
+
+    case 4:
+        printf("\n*-*-*-*-*-*Eliminar contacto-*-*-*-*-*");
+        printf("\n*-*-*-*-*-*Work in progress*-*-*-*-*-*");
+        break;
+
+    case 5:
+        printf("\n*-*-*-*-*-*Listar contactos*-*-*-*-*-*");
+        printf("\n*-*-*-*-*-*Work in progress*-*-*-*-*-*");
+        break;
+
+    case 6:
+        printf("\n*-*-*-*-*-*-*-*-*Salir-*-*-*-*-*-*-*-*");
+        exit;
+        break;
+    }
 
 }
 
